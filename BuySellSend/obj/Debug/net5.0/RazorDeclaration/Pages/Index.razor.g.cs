575eccb9923a1413e21e6305d3e534cf03cbc49d
Yauchin M. Lam <yauchinlam17@gmail.com>
2021-07-18 23:24:18 -0400

    changed file path name
    Changed file path name

--- BuySellSend/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ BuySellSend/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,91 +13,91 @@ namespace BuySellSend.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 1 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 2 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 3 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 4 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 5 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 6 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 7 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 8 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 9 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using BuySellSend;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 10 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using BuySellSend.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
+#line 2 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
 using BuySell.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
+#line 3 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
 using BuySell;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
+#line 4 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
 using BuySellSend.Services;
 
 #line default
@@ -112,7 +112,7 @@ using BuySellSend.Services;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 24 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
+#line 24 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\Pages\Index.razor"
       
     private StockRequestModel stockRequest = new StockRequestModel();
     
--- BuySellSend/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ BuySellSend/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace BuySellSend.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 1 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 2 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 3 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 4 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 5 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 6 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 7 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 8 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 9 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using BuySellSend;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
+#line 10 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\_Imports.razor"
 using BuySellSend.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using BuySellSend.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Gonzalez\Documents\GitHub\BuySell\BuySellSend\Shared\SurveyPrompt.razor"
+#line 12 "C:\Users\Yauch\Documents\GitHub\BuySell\BuySellSend\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
